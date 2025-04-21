d5465bac780cedd835676fe07c4313a998b7651f
AnishyaV <114719596+AnishyaV@users.noreply.github.com>
2025-04-21 10:14:16 +0530

    Update DBConnection.java

--- com/foodordering/util/DBConnection.java
+++ com/foodordering/util/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/food_ordering";
     private static final String USER = "root";
-    private static final String PASSWORD = "Ani@2004";
+    private static final String PASSWORD = "password";
 
     static {
         try {
